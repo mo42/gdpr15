66fc452ee88df8c7cd697abd913d013db9a9cdc8
Moritz Winter <me@mo42.de>
2018-05-24 11:21:28 +0200

    Rename files

--- dsgvo15.py
+++ dsgvo15.py
@@ -33,7 +33,7 @@ class Letter(object):
     def __init__(self, institution_json, requester_json):
         self.institution = Institution(institution_json)
         self.requester = Requester(requester_json)
-        text = open('bdsg34.txt', 'r')
+        text = open('dsgvo15.txt', 'r')
         self.text = text.read()
 
     def to_tex(self):
