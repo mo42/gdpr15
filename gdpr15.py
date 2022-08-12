1e1c18947366f07f6b1079c91df6bb091c52451d
Moritz Winter <me@mo42.de>
2022-08-12 19:50:20 +0200

    Escape LaTeX characters

--- gdpr15.py
+++ gdpr15.py
@@ -7,24 +7,47 @@ import subprocess
 import sys
 from string import Template
 
+ESCAPE_CHARACTERS = ['\\', '&', '%', '$', '#', '_', '{', '}', '~', '^']
+
+def tex_escape(string):
+    for escape_character in ESCAPE_CHARACTERS:
+        string = string.replace(escape_character, f'\{escape_character}')
+    return string
+
+def clean_file_name(string):
+    for escape_character in ESCAPE_CHARACTERS:
+        string = string.replace(escape_character, '')
+    return string.lower().replace(' ', '_')
+
+def tex_escape_dict_values(d):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            tex_escape_dict_values(v)
+        else:
+            if isinstance(v, str):
+                d[k] = tex_escape(v)
+    return d
+
 def main(file_name):
     '''Print LaTeX letter of the request on the command line.'''
     try:
         with open(file_name, mode='r', encoding='utf-8') as institution_file, \
             open('config.json', mode='r', encoding='utf-8') as requester_file:
-            institution = json.loads(institution_file.read())
+            institution = tex_escape_dict_values(
+                json.loads(institution_file.read())
+            )
             requester = json.loads(requester_file.read())
     except IOError:
         print(f'Error: {file_name} or \'config.json\' do(es) not exist.')
         raise
     data = institution['company']
     data.update(requester)
-    name = institution['company']['name'].lower().replace(' ', '_')
+    file_name = clean_file_name(institution['company']['name'])
     with open('letter.template', mode='r', encoding='utf-8') as template_file, \
-        open(f'{name}.tex', mode='w', encoding='utf-8') as letter_file:
+        open(f'{file_name}.tex', mode='w', encoding='utf-8') as letter_file:
         template = Template(template_file.read())
         letter_file.write(template.substitute(data))
-    subprocess.call(['lualatex', f'{name}.tex'])
+    subprocess.call(['lualatex', f'{file_name}.tex'])
 
 
 if __name__ == '__main__':
